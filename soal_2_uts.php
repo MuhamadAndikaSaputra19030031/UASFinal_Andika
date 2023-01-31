99ef9ff91de19f24095a1c7b678a721c36371058
Muhamad Andika Saputra <102354743+MuhamadAndikaSaputra19030031@users.noreply.github.com>
2023-02-01 06:48:00 +0700

    Add files via upload

---
+++ soal_2_uts.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Latihan2-3</title>
+</head>
+<body>
+<br>Paragraf ini pada posisi rata kiri (default)</br>
+<p align="center">Paragraf ini pada posisi rata tengah</br>
+<p align="right">Paragraf ini pada posisi rata kanan</br>
+<p align="left">Paragraf ini pada posisi rata kiri</br>
+</body>
+</html>
